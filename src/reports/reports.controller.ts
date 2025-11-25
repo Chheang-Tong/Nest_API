ec64b7df7f661ac7a249d24d2ba2cea799ad700a
Chheang-Tong <chheang.tong2@gmail.com>
2025-11-25 14:04:51 +0700

    report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guard';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+@UseGuards(JwtAuthGuard) // you can limit to admin later
+export class ReportsController {
+  constructor(private readonly reportService: ReportsService) {}
+
+  // 🔹 GET /reports/stock
+  @Get('stock')
+  getStockReport() {
+    return this.reportService.getStockReport();
+  }
+
+  // 🔹 GET /reports/sales?from=2025-11-01&to=2025-11-30
+  @Get('sales')
+  getSalesReport(@Query('from') from?: string, @Query('to') to?: string) {
+    return this.reportService.getSalesReport(from, to);
+  }
+}
